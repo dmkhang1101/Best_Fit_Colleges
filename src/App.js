0d2129499e00148c196c75ebf00e54d4500681f2
dmkhang1101 <114703634+dmkhang1101@users.noreply.github.com>
2024-08-11 13:22:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,9 +51,19 @@ function Header (){
     };
 
     const handleCloseUserMenu = (setting) => {
+        if (setting === 'Logout'){
+            user.setLogIn(false)
+            navigate('/')
+        }
+        // if (setting === 'Saved Searches'){
+        //     navigate('../saved-searches')
+        // }
+        // if (setting === 'Saved School Lists'){
+        //     navigate('../saved-school-lists')
+        // }
         setAnchorElUser(null);
     };
-        
+
     const handleReset = (page) => {
         if (page === "Home"){
             navigate("/")
@@ -129,7 +139,7 @@ function Header (){
                             onClose={handleCloseUserMenu}
                             >
                             {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                <MenuItem key={setting} onClick={() => handleCloseUserMenu(setting)}>
                                     <Typography textAlign="center">{setting}</Typography>
                                 </MenuItem>
                             ))}
